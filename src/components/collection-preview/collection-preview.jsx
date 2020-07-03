e39b48c8c043e008a8da60979542a330efdcd316
Chris Chandler <chris@chrischandler.com>
2020-07-03 14:56:46 +0800

    Added shop page

---
+++ src/components/collection-preview/collection-preview.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <h2>item</h2>
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
