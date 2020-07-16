86069173b9a1dee9caa7dbca05b015e62347a733
Chris Chandler <chris@chrischandler.com>
2020-07-16 22:47:15 +0800

    Multiple updates to collection-item, collections-overview, directory, collection

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 
 //Library for state management and the store that's created to manage the state
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
@@ -16,7 +17,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
