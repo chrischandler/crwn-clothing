d86f1c30adcf0a278412838fb0157fdd36d87dfd
Chris Chandler <chris@chrischandler.com>
2020-07-22 17:15:30 +0800

    Updated shop-saga to match pattern of cart and user sagas

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,10 +1,11 @@
 import { all, call } from "redux-saga/effects";
 
 // Import any saga you want to listen for here
-import { fetchCollectionsStart } from "./shop/shop.sagas";
+import { cartSagas } from "./shop/shop.sagas";
 import { userSagas } from "./user/user.sagas";
+import { cartSagas } from "./cart/cart.sagas";
 
 // Add the imported saga to the array
 export default function* rootSaga() {
-  yield all([call(fetchCollectionsStart), call(userSagas)]);
+  yield all([call(cartSagas), call(userSagas), call(cartSagas)]);
 }
